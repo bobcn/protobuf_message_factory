eba8981683b3622af371185c31efb6d57161600f
bobcn <31151645+bobcn@users.noreply.github.com>
2020-02-02 15:19:44 +0800

    Update lib.rs
    现在需要在每个 message 的上方填写 id，id 为 u16 类型：
    ```protobuf
    // @id: 0xffff
    message AMessage {}
    ```
    现在生成的 factory 可以：
    1. 根据 message id 获得 MessageDescriptor；
    2. 根据 MessageDescriptor 获得 message id；

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,18 @@ use std::fs::File;
 use std::io::{BufRead, BufReader, Read, Write};
 use std::path::PathBuf;
 
+#[derive(Debug)]
+pub struct MessageMetadata {
+    name: String,
+    id: String,
+}
+
 /// protobuf message and file info
 #[derive(Debug)]
 pub struct ProtoMessageInfo {
-    file: PathBuf,
+    file_path: PathBuf,
     file_name: String,
-    messages: Vec<String>,
+    messages: Vec<MessageMetadata>,
 }
 
 /// get protobuf message and file info
@@ -19,7 +25,8 @@ pub fn get_protos_info(p: &str) -> Vec<ProtoMessageInfo> {
     let mut v = Vec::new();
     let mut path = p.to_string();
 
-    let re = Regex::new(r"message\s+([^\s]+)\s*\{*$").unwrap();
+    let msg_id_re = Regex::new(r"//\s*@id:\s*(0[xX][0-9a-fA-F]{4})\s*$").unwrap();
+    let msg_name_re = Regex::new(r"message\s+([^\s]+)\s*\{*$").unwrap();
 
     path.push_str("/*.proto");
 
@@ -28,15 +35,23 @@ pub fn get_protos_info(p: &str) -> Vec<ProtoMessageInfo> {
             let f = File::open(path.clone()).expect("Failed to open file");
             let reader = BufReader::new(f);
             let mut item = ProtoMessageInfo {
-                file: path.clone(),
+                file_path: path.clone(),
                 file_name: path.file_stem().unwrap().to_str().unwrap().to_string(),
                 messages: vec![],
             };
 
+            let mut msg_id: Option<String> = None;
             for line in reader.lines() {
-                for caps in re.captures_iter(line.unwrap().as_str()) {
-                    item.messages
-                        .push(caps.get(1).unwrap().as_str().to_string());
+                if let None = msg_id {
+                    for caps in msg_id_re.captures_iter(line.unwrap().as_str()) {
+                        msg_id = Some(caps.get(1).unwrap().as_str().to_string());
+                    }
+                    continue
+                }
+                for caps in msg_name_re.captures_iter(line.unwrap().as_str()) {
+                    let name = caps.get(1).unwrap().as_str().to_string();
+                    item.messages.push(MessageMetadata { name, id: msg_id.unwrap() });
+                    msg_id = None;
                 }
             }
             v.push(item)
@@ -46,6 +61,15 @@ pub fn get_protos_info(p: &str) -> Vec<ProtoMessageInfo> {
     v
 }
 
+/// get proto's filename list
+pub fn get_proto_list(v: &Vec<ProtoMessageInfo>) -> Vec<&str> {
+    let mut r = Vec::new();
+    for f in v.iter() {
+        r.push(f.file_path.to_str().unwrap());
+    }
+    r
+}
+
 /// generate factory into `path`
 pub fn generate_factory_file(path: &str, v: &Vec<ProtoMessageInfo>) {
     let mut contents = "use std::cell::RefCell;
@@ -53,23 +77,32 @@ use std::collections::HashMap;
 use protobuf::reflect::MessageDescriptor;
 use protobuf::Message;
 
+pub type MessageId = u16;
 
 thread_local! {
-    pub static GLOBAL_MAP : RefCell<HashMap<String, &'static MessageDescriptor>> = RefCell::new(HashMap::new());
+    pub static NAME_ID_MAP : RefCell<HashMap<String, MessageId>> = RefCell::new(HashMap::new());
+    pub static ID_DESC_MAP : RefCell<HashMap<MessageId, &'static MessageDescriptor>> = RefCell::new(HashMap::new());
 }
 
-pub fn register_message<M: Message>() {
-    GLOBAL_MAP.with(|x| {
+pub fn register_message<M: Message>(id: MessageId) {
+    NAME_ID_MAP.with(|x| {
         let mut m = x.borrow_mut();
         let name = M::descriptor_static().full_name().to_string();
         if !m.contains_key(&name) {
-            m.insert(name, M::descriptor_static());
+            m.insert(name, id);
+        }
+    });
+    ID_DESC_MAP.with(|x| {
+        let mut m = x.borrow_mut();
+        let name = M::descriptor_static().full_name().to_string();
+        if !m.contains_key(&id) {
+            m.insert(id, M::descriptor_static());
         }
     })
 }
 
-pub fn get_descriptor(full_name: &String) -> Option<&'static MessageDescriptor> {
-    GLOBAL_MAP.with(move |x| {
+pub fn get_id(msg_desc: &MessageDescriptor) -> Option<MessageId> {
+    NAME_ID_MAP.with(|x| {
         {
             let m = x.borrow_mut();
             if m.len() == 0 {
@@ -79,7 +112,26 @@ pub fn get_descriptor(full_name: &String) -> Option<&'static MessageDescriptor> 
         }
         {
             let m = x.borrow_mut();
-            match m.get(full_name) {
+            match m.get(msg_desc.full_name()) {
+                Some(message_id) => Some(message_id.clone()),
+                None => None,
+            }
+        }
+    })
+}
+
+pub fn get_descriptor(id: MessageId) -> Option<&'static MessageDescriptor> {
+    ID_DESC_MAP.with(move |x| {
+        {
+            let m = x.borrow_mut();
+            if m.len() == 0 {
+                drop(m);
+                init_descriptors()
+            }
+        }
+        {
+            let m = x.borrow_mut();
+            match m.get(&id) {
                 Some(r) => Some(*r),
                 None => None,
             }
@@ -87,7 +139,7 @@ pub fn get_descriptor(full_name: &String) -> Option<&'static MessageDescriptor> 
     })
 }".to_string().into_bytes();
 
-    let mut mod_file = File::create((path.to_string() + "/lib.rs").as_str()).unwrap();
+    let mut mod_file = File::create((path.to_string() + "/../protos.rs").as_str()).unwrap();
     let mut factory_file = File::create((path.to_string() + "/factory.rs").as_str()).unwrap();
 
     mod_file.write(b"pub mod factory;\n");
@@ -107,43 +159,21 @@ pub fn get_descriptor(full_name: &String) -> Option<&'static MessageDescriptor> 
 
     factory_file.write(b"\nfn init_descriptors() {");
 
+    mod_file.write_fmt(format_args!(
+        "{}\n{}\n", "pub use factory::get_id;", "pub use factory::get_descriptor;"
+    ));
+
     for file in v.iter() {
         for msg in file.messages.iter() {
             factory_file.write_fmt(format_args!(
-                "\n    register_message::<{}::{}>();",
-                file.file_name, msg
+                "\n    register_message::<{}::{}>({});",
+                file.file_name, msg.name, msg.id
+            ));
+            mod_file.write_fmt(format_args!(
+                "pub use {}::{};\n", file.file_name, msg.name
             ));
         }
     }
 
     factory_file.write(b"\n}\n");
 }
-
-/// get proto's filename list
-pub fn get_proto_list(v: &Vec<ProtoMessageInfo>) -> Vec<&str> {
-    let mut r = Vec::new();
-
-    for f in v.iter() {
-        r.push(f.file.to_str().unwrap());
-    }
-
-    r
-}
-
-//fn main() {
-//    let proto_path = "src/";
-//
-//    let v = get_protos_info(proto_path);
-//    let protos = get_proto_list(&v);
-//
-//    protoc_rust::run(protoc_rust::Args {
-//        out_dir: proto_path,
-//        input: &protos,
-//        includes: &[proto_path],
-//        customize: Customize {
-//          ..Default::default()
-//        },
-//    }).expect("protoc");
-//
-//    generate_factory_file(proto_path, &v);
-//}
